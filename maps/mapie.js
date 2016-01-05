ed66a7c8ff0d4571aed940e628e97a5a60d74d81
aaravind <aravindarivarasan@gmail.com>
2016-01-05 11:44:02 +0530

    commit customizing colors in maps
    commit customizing colors in maps

--- maps/mapie.js
+++ maps/mapie.js
@@ -59,6 +59,100 @@
         }
         var map = new google.maps.Map(mapCanvas, mapOptions);
         map.setTilt(45);
+
+        map.set('styles', [
+   {
+       "featureType": "administrative",
+       "elementType": "labels.text.fill",
+       "stylers": [
+           {
+               "color": "#444444"
+           }
+       ]
+   },
+   {
+       "featureType": "landscape",
+       "elementType": "all",
+       "stylers": [
+           {
+               "color": "#f2f2f2"
+           }
+       ]
+   },
+   {
+       "featureType": "poi",
+       "elementType": "all",
+       "stylers": [
+           {
+               "visibility": "off"
+           }
+       ]
+   },
+   {
+       "featureType": "poi.park",
+       "elementType": "geometry.fill",
+       "stylers": [
+           {
+               "visibility": "on"
+           },
+           {
+               "color": "#dfe9db"
+           }
+       ]
+   },
+   {
+       "featureType": "road",
+       "elementType": "all",
+       "stylers": [
+           {
+               "saturation": -100
+           },
+           {
+               "lightness": 45
+           }
+       ]
+   },
+   {
+       "featureType": "road.highway",
+       "elementType": "all",
+       "stylers": [
+           {
+               "visibility": "simplified"
+           }
+       ]
+   },
+   {
+       "featureType": "road.arterial",
+       "elementType": "labels.icon",
+       "stylers": [
+           {
+               "visibility": "off"
+           }
+       ]
+   },
+   {
+       "featureType": "transit",
+       "elementType": "all",
+       "stylers": [
+           {
+               "visibility": "off"
+           }
+       ]
+   },
+   {
+       "featureType": "water",
+       "elementType": "all",
+       "stylers": [
+           {
+               "color": "#c4eaf9"
+           },
+           {
+               "visibility": "on"
+           }
+       ]
+   }
+]);
+
         //map.controls[google.maps.ControlPosition.RIGHT_TOP].push(
   //document.getElementById('map_legend'));
           $(function() {
