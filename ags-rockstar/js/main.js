c40e91656dcb09fdb2f05ecae4759df6c10d442e
aaravind <aravindarivarasan@gmail.com>
2018-01-19 00:00:17 +0530

    counter,modal,2 card

--- ags-rockstar/js/main.js
+++ ags-rockstar/js/main.js
@@ -17,3 +17,29 @@ $('.carousel').carousel({
   interval: 3000,
   ride:true
 })
+var days, hours, minutes, seconds;
+/* Counter code*/
+function callCounter(id){
+	dateVal = $('#'+id).attr("data-date");
+	target_date = new Date(dateVal).getTime();
+	countdown = document.getElementById(id);
+	setInterval(function () {
+	    var current_date = new Date().getTime();
+	    var seconds_left = (target_date - current_date) / 1000;
+	    days = parseInt(seconds_left / 86400);
+	    seconds_left = seconds_left % 86400;   
+	    hours = parseInt(seconds_left / 3600);
+	    seconds_left = seconds_left % 3600;     
+	    minutes = parseInt(seconds_left / 60);
+	    seconds = parseInt(seconds_left % 60);
+	    countdown.innerHTML = '<span class="days">' + (days.toString().length == 1 ? ('0'+days) : days) +  '</span> <span class="hours">' + (hours.toString().length == 1 ? ('0'+hours) : hours)  + '</span> <span class="minutes">'
+	    + (minutes.toString().length == 1 ? ('0'+minutes) : minutes) + '</span> <span class="seconds">' + (seconds.toString().length == 1 ? ('0'+seconds) : seconds)  + '</span>';  
+	 
+	}, 1000);
+}
+callCounter('countdown');
+/* End of counter code */
+
+    $( document ).ready(function() {
+ $('#myModal').modal('show');
+});
