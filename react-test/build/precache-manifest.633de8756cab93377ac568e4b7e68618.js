a5bc225a241e2e353ad8320a69401c4312363a79
aaravind <aravindarivarasan@gmail.com>
2020-03-05 13:20:50 +0530

    push build

---
+++ react-test/build/precache-manifest.633de8756cab93377ac568e4b7e68618.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "faec57beeab9a68fdc93664a6725bcd3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3a76d4836a5d9cacb6d2",
+    "url": "/static/css/main.5d6474b7.chunk.css"
+  },
+  {
+    "revision": "0445078e052d91f254fc",
+    "url": "/static/js/2.261abf83.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/static/js/2.261abf83.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3a76d4836a5d9cacb6d2",
+    "url": "/static/js/main.894512e8.chunk.js"
+  },
+  {
+    "revision": "f6e3084cbc954775afac",
+    "url": "/static/js/runtime-main.aa3ef424.js"
+  }
+]);
